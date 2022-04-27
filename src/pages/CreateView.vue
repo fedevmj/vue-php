07cffd9bcfcc364820d485af84ee21ee4900fa41
mj <dev.mjleemj@gmail.com>
2022-04-27 15:08:59 +0900

    delete 기능추가

--- src/pages/CreateView.vue
+++ src/pages/CreateView.vue
@@ -1,9 +1,70 @@
 <template>
-    <h1>Create</h1>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                TODO Create
+            </div>
+            <div class="card-body">
+                <form @submit.prevent="onSubmit">
+                    <div class="form-group">
+                        <label for="title">제목</label>
+                        <input type="text" v-model="todo.title" class="form-control" name="title" id="title" aria-describedby="helpId" placeholder="">
+                        <small id="helpId" class="form-text text-muted">제목을 입력하세요.</small>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="body"></label>
+                        <textarea class="form-control" v-model="todo.body" name="body" id="body" rows="3"></textarea>
+                        <small id="helpId" class="form-text text-muted">내용을 입력하세요.</small>
+                    </div>
+
+                    <div class="btn-group" role="group">
+                        <button type="submit" class="btn btn-success">확인</button>
+                        <button type="button" class="btn btn-warning">취소</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
     
 <script>
+    import {reactive} from 'vue'
+    import { useRouter } from 'vue-router'
+    export default{
+
+        setup(){
+            // 값이 변하는 것을 계속 유지.
+            const todo = reactive({
+                title: '',
+                body: ''
+            })
+
+            const router = useRouter();
+
+            const onSubmit = () => {
+                console.log(todo);
+                // ?를 기준으로 앞이 주소, 뒤가 query
+                // &은 변수 구분 (title, body)
+                fetch(`http://mjleemj.dothome.co.kr/data_add.php?title=${todo.title}&body=${todo.body}`)
+                .then(res => res.json())
+                .then(data => {
+                    if(data.result == 1){
+                        // List 화면으로 이동
+                        router.push('/list');
+                    }else{
+                        console.log('서버에서 자료를 불러오는 데 실패했습니다.')
+                    }
+                })
+                .catch()
+            }
+            return{
+                todo,
+                onSubmit
+            }
+        }
     
+    }
 </script>
     
 <style>
--- src/pages/ListView.vue
+++ src/pages/ListView.vue
@@ -1,11 +1,111 @@
 <template>
-    <h1>List</h1>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Todo List
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <!-- <th>ID</th> -->
+                            <th>Title</th>
+                            <!-- <th>Body</th> -->
+                            <th>Complete</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr 
+                        v-for="(item, index) in todos"
+                        :key="item.id"
+                        >
+                            <th>{{ index + 1 }}</th>
+                            <!-- <td>{{ item.id }}</td> -->
+                            <td><span @click="moveDetail(item.id)" class="detail">{{ item.title }}</span></td>
+                            <!-- <td>{{ item.body }}</td> -->
+                            <td>{{ item.complete }}</td>
+                            <td>
+                                <div class="btn-group" role="group">
+                                    <button class="btn btn-primary">편집</button>
+                                    <button class="btn btn-danger" @click="deleteTodo(item.id)">삭제</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </template>
-    
+
 <script>
-    
+    import {ref} from 'vue'
+    import {useRouter} from 'vue-router'
+
+    export default {
+
+        setup() {
+            // 자료 보관 배열
+            const todos =ref([])
+            // 서버에서 자료를 읽어오기
+            const router = useRouter();
+
+            const getInfo = () => {
+                fetch('http://mjleemj.dothome.co.kr/data_read.php')
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data.result);
+                        todos.value = data.result;
+                    })
+                    .catch()
+            }
+            getInfo();
+
+            const deleteTodo = (id) => {
+                console.log(id);
+                fetch(`http://mjleemj.dothome.co.kr/data_delete.php?id=${id}`)
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if(data.result == 1){
+                            getInfo();
+                        }else{
+                            console.log('삭제에 실패했습니다')
+                        }
+                    })
+            }
+
+            const moveDetail = (id) => {
+                console.log(id);
+                router.push({
+                    name: 'Detail',
+                    params: {
+                        id : id
+                    }
+                })
+            }
+            return {
+                todos,
+                deleteTodo,
+                moveDetail
+            }
+        }
+
+    }
 </script>
-    
-<style>
-    
+
+<style scoped>
+
+.detail{
+    cursor: pointer;
+    color: #000;
+}
+
+.detail:hover{
+    text-decoration: underline;
+    color: green;
+}
+
 </style>
